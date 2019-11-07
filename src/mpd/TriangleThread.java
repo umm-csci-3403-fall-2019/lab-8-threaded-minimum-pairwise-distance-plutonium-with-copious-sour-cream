6915469ca5ebe5ad0f89fabc99b6922b62115fcf
deglm006 <deglm006@morris.umn.edu>
2019-11-07 10:46:32 -0600

    Write abstract class for our threads

---
+++ src/mpd/TriangleThread.java
@@ -0,0 +1,18 @@
+package mpd;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+abstract class TriangleThread {
+
+  /*
+  uses an AtomicLong and a spinloop to try and commit our value to the AtomicLong
+   */
+  public void report(Long ourValue, AtomicLong externalValue){
+    Long localValue = externalValue.get();
+      while ((localValue > ourValue) &&
+              (!externalValue.compareAndSet(localValue,ourValue))){
+        localValue = externalValue.get();
+      }
+    }
+  }
+
