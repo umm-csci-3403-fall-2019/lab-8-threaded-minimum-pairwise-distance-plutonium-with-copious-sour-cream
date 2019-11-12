c3de67eaa0850cd286bc6edd1f95963a22f59d71
deglm006 <deglm006@morris.umn.edu>
2019-11-12 10:34:09 -0600

    Change things to longs in serial MPD to make code work for edge cases

--- src/mpd/SerialMinimumPairwiseDistance.java
+++ src/mpd/SerialMinimumPairwiseDistance.java
@@ -4,11 +4,11 @@ public class SerialMinimumPairwiseDistance implements MinimumPairwiseDistance {
 
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        long result = Integer.MAX_VALUE;
+        long result = Long.MAX_VALUE;
         for (int i = 0; i < values.length; ++i) {
             for (int j = 0; j < i; ++j) {
                 // Gives us all the pairs (i, j) where 0 <= j < i < values.length
-                long diff = Math.abs(values[i] - values[j]);
+                long diff = Math.abs((long)values[i] - values[j]);
                 if (diff < result) {
                     result = diff;
                 }
